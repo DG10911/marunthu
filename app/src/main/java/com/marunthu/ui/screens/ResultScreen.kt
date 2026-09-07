7b54c359c093c729c2906b534e31930c3584f62e
DG10911 <pulkitmlk11@gmail.com>
2026-09-07 12:02:29 +0530

    Fix: add missing sp import in ResultScreen (build was failing)

--- app/src/main/java/com/marunthu/ui/screens/ResultScreen.kt
+++ app/src/main/java/com/marunthu/ui/screens/ResultScreen.kt
@@ -39,6 +39,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.marunthu.core.model.SafetyStatus
 import com.marunthu.data.MedicineAdvisor
 import com.marunthu.data.MedicineInfo
